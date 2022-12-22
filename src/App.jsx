6a9a7a05eb6174d0716a1d5abb229f96bda0f78d
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-12-22 16:41:44 +0900

    Add sort & default note feature

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,10 @@ function App() {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
+  useEffect(() => {
+    setActiveNote(notes[0].id);
+  }, []);
+
   const onAddNote = () => {
     const newNote = {
       id: uuidv4(),
